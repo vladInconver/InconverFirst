9dc52656185a15cb63ff4848518d5c1d3ef50de2
vladInconve <vlad@inconver.com>
2016-10-16 22:43:11 +0300

    1
    im

--- inconver_first/404.php
+++ inconver_first/404.php
@@ -8,7 +8,7 @@
             <div class="contact-content">
 
                 <h1 class="errorh1"><?php _e('Error, Page not found','aletheme'); ?></h1>
-                <p class="errorh1"><?php _e('Sorry, but the page you\'re looking for has not found. Try checking the URL for errors, then hit the refresh<br /> button on your browser.','aletheme'); ?></p>
+                <p class="errorh1 qqq"><?php _e('Sorry, but the page you\'re looking for has not found. Try checking the URL for errors, then hit the refresh<br /> button on your browser.','aletheme'); ?></p>
 
             </div>
 
